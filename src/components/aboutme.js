b8c7180613605a28cdedc8ed049456da82168ea9
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-09 21:36:23 +0700

    add commit

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,4 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, useState  } from 'react';
+import "../App.css";
+import "antd/dist/antd.css";
+import { Row, Col, Button, Divider } from "antd";
+import Bio from "./Bio";
+import Skill from "./Skill";
+import Participated from "./Participated";
+import ProfileImg from "./ProfileImg";
+import Contributions from "./Contributions";
+import Internship from "./Internship";
+import Workshop from "./Workshop";
+
 
 
 class About extends Component {
